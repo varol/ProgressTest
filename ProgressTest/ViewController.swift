205814b0afae306515c91debcf17ba0ef5cedf25
varolaksoy <varolaksoy@gmail.com>
2019-09-10 22:01:54 +0300

    first commit

--- ProgressTest/ViewController.swift
+++ ProgressTest/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var progressBar: ProgressBar!
+    
+    var countFired: CGFloat = 180
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+ 
+        
+        Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { (timer) in
+            self.countFired -= 1
+            print(self.countFired)
+            
+            
+            DispatchQueue.main.async {
+                self.progressBar.progress = min(self.countFired, 1)
+
+                if self.progressBar.progress == 0 {
+                    timer.invalidate()
+                }
+            }
+        }
+        
+        
+
     }
-
-
+    
 }
 

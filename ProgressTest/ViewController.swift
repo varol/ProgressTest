ffe0e51b7f4e91aebce20f70abb0087c74e5115f
varolaksoy <varolaksoy@gmail.com>
2019-09-10 20:21:18 +0300

    Initial Commit

---
+++ ProgressTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgressTest
+//
+//  Created by Mac on 10.09.2019.
+//  Copyright © 2019 Varol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
